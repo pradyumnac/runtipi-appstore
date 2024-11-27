71aa9411c39da01d40f8681c5f10604bcb62721e
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2024-11-27 09:58:39 +0530

    Working authentik
    Signed-off-by: Pradyumna Chatterjee <chatterjee.pra@gmail.com>

--- templates/pg.init.d/multidb.init.sh
+++ templates/pg.init.d/multidb.init.sh
@@ -6,9 +6,11 @@ set -u
 function create_user_and_database() {
 	local database=$(echo $1 | tr ',' ' ' | awk  '{print $1}')
 	local owner=$(echo $1 | tr ',' ' ' | awk  '{print $2}')
+	local password=$(echo $1 | tr ',' ' ' | awk  '{print $3}')
 	echo "  Creating user and database '$database'"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE USER $owner IF NOT EXISTS;
+	# If volume exists, will not e executes
+	PGPASSWORD=$POSTGRES_PASSWORD psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+		CREATE ROLE $owner LOGIN PASSWORD '$password';
 	    CREATE DATABASE $database;
 	    GRANT ALL PRIVILEGES ON DATABASE $database TO $owner;
 EOSQL
